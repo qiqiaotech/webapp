14e91265cacbb56c9c810ad9cf3c92a6cc4ca1a8
qiqiaotech <qiqiaotech@qq.com>
2019-06-10 11:05:35 +0800

    vue

---
+++ src/main/webapp/static/components/Child2.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="child-2">
+        <p>in child2:</p>
+        <p>props: {{pChild2}}</p>
+        <p>$attrs: {{$attrs}}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Child2',
+        props:['pChild2'],
+        inheritAttrs: false,
+        mounted() {
+            this.$emit('test2')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
